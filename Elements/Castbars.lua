b766c686736f285ccbe2995c5c7d4ad48af815a8
frumpymoons <pngmn@mailbox.org>
2019-12-27 04:15:07 +0100

    Don't use number font for castbar

--- Elements/Castbars.lua
+++ Elements/Castbars.lua
@@ -171,12 +171,14 @@ function ns.CreateCastbars(self)
 		Castbar.Icon = Icon
 	end
 
-	Castbar.Time = ns.CreateFontString(Castbar, 13, 'RIGHT')
+	Castbar.Time = ns.CreateFontStringBig(Castbar, 13, 'RIGHT')
 	Castbar.Time:SetPoint('RIGHT', Castbar, -5, -1)
+	Castbar.Time:SetFont(ns.config.fontBig, 12, ns.config.fontBigOutline)
 
-	Castbar.Text = ns.CreateFontString(Castbar, 13, 'LEFT')
+	Castbar.Text = ns.CreateFontStringBig(Castbar, 13, 'LEFT')
 	Castbar.Text:SetPoint('LEFT', Castbar, 4, -1)
 	Castbar.Text:SetPoint('RIGHT', Castbar, 'RIGHT', -40, -1)
+	Castbar.Text:SetFont(ns.config.fontBig, 12, ns.config.fontBigOutline)
 	Castbar.Text:SetWordWrap(false)
 
 	Castbar.PostCastStart = ns.PostCastStart
